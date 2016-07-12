e71734a2b008861d96dd3ed43d6a3d6e86b9e70e
trikitrok <trikitrok@gmail.com>
2016-07-12 17:23:06 +0200

    adding component library

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject bank-account "0.0.1-SNAPSHOT"
   :description "Bank Account kata"
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.stuartsierra/component "0.3.1"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]]}
              ;; You can add dependencies that apply to `lein midje` below.
              ;; An example would be changing the logging destination for test runs.
