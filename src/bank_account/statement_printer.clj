94ef59b28a7840a30b945cad267d26fe46f0b421
trikitrok <trikitrok@gmail.com>
2016-07-24 23:26:06 +0200

    separating formatting and printing

--- src/bank_account/statement_printer.clj
+++ src/bank_account/statement_printer.clj
@@ -8,12 +8,19 @@
 (defn- print-header []
   (println "date || credit || debit || balance"))
 
+(defn- print-lines [lines]
+  (doall (map println lines)))
+
+(defn- format-and-print-lines [lines]
+  (->> lines
+       (map formatting/format-statement-line)
+       print-lines))
+
 (defrecord ConsoleStatementPrinter []
   StatementPrinter
   (print-statement [_ statement-lines]
     (print-header)
-    (doseq [line statement-lines]
-      (println (formatting/format-statement-line line)))))
+    (format-and-print-lines statement-lines)))
 
 (defn use-console-printer []
   (println ";; creating ConsoleStatementPrinter")
